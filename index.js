ae2006f314188bf9d03b25d5aeb5eb253f9afe2d
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-02-13 20:34:26 +0100

    Make webpage responsive to phone

---
+++ index.js
@@ -0,0 +1,6 @@
+const  passwordInput = document.getElementById("password");
+const passwordConfirmInput = document.getElementById("confirm-password");
+const submitButton = document.querySelector(".submit-button");
+const passwordError = document.querySelector("#password + span");
+
+
