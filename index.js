d10b8b9d2cc0c55fc4cfa9c5035c32d1d60dd0fd
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-02-14 14:50:54 +0100

    Add error message when passwords don't match

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+
+//DOM elements
+
 const  passwordInput = document.getElementById("password");
 const passwordConfirmInput = document.getElementById("confirm-password");
 const submitButton = document.querySelector(".submit-button");
-const passwordError = document.querySelector("#password + span");
+const passwordError = document.querySelector(".password-error");
+
+let visible = false;
+//
+passwordConfirmInput.addEventListener("input",()=>{
+    
+    if(passwordInput.value !== passwordConfirmInput.value){
+        
+        //prevent the default behavior that is to submit the form
+        
+        if(!visible){
+            passwordError.classList.remove("invisible");
+            passwordError.classList.add("visible");
+            visible = true;
+
+        }
+
+    }
+    else if (passwordConfirmInput.value === passwordInput.value){
+        if(visible){
+            passwordError.classList.remove("visible");
+            passwordError.classList.add("invisible");
+            visible = false;
+        }
+    }
+});
+
+
+submitButton.addEventListener("click",(e)=>{
+    if(passwordInput.value !== passwordConfirmInput.value){
+        e.preventDefault();
+    }
+})
 
 
